f1ac5a788e015f137d6d0251557a2215de165385
David Gustafsson <david.gustafsson@ub.gu.se>
2023-11-20 16:26:54 +0100

    Upgrade rails and fix docker config

---
+++ docker/build/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source .env
+
+# Overwrite GIT_REVISION with first argument if passed
+if [ -n "$1" ]; then
+  GIT_REVISION=$1
+fi
+
+docker push docker.ub.gu.se/openhours:${GIT_REVISION}
